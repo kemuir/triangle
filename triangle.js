8daf3fd273ad6f2f3ae11615b8761a1ef549d643
Katherine Koopmans <katherineemuir@gmail.com>
2024-12-31 13:59:04 -0500

    scale and centre

--- triangle.js
+++ triangle.js
@@ -26,20 +26,30 @@ export function generateTriangle(){
     const angB = (ang2 != angMin && ang2 != angMax) ? (180 - Math.round((Math.abs(ang1-ang3)/2)*180/Math.PI)) : (Math.round((Math.abs(ang1-ang3)/2)*180/Math.PI)) 
     const angC = (ang3 != angMin && ang3 != angMax) ? (180 - Math.round((Math.abs(ang2-ang1)/2)*180/Math.PI)) : (Math.round((Math.abs(ang2-ang1)/2)*180/Math.PI))
 
-    ctx.textAlign = "center"
+    ctx.strokeStyle = "red"
+    ctx.beginPath();
+    ctx.moveTo(pointA.x,pointA.y);
+    ctx.lineTo(pointB.x,pointB.y);
+    ctx.lineTo(pointC.x,pointC.y);
+    ctx.lineTo(pointA.x,pointA.y);
+    ctx.stroke();
 
+    scaleAndCentre(pointA,pointB,pointC)
+
+    ctx.textAlign = "center"
+    ctx.strokeStyle = "black"
     ctx.lineWidth = 3;
     ctx.lineCap = "round";
     ctx.beginPath();
-    ctx.moveTo(x1,y1);
-    ctx.lineTo(x2,y2);
-    ctx.lineTo(x3,y3);
-    ctx.lineTo(x1,y1);
+    ctx.moveTo(pointA.x,pointA.y);
+    ctx.lineTo(pointB.x,pointB.y);
+    ctx.lineTo(pointC.x,pointC.y);
+    ctx.lineTo(pointA.x,pointA.y);
     ctx.stroke();
 
-    ctx.fillText("A:"+angA,x1+20,y1+20)
-    ctx.fillText("B:"+angB,x2+20,y2+20)
-    ctx.fillText("C:"+angC,x3+20,y3+20)
+    ctx.fillText("A:"+angA,20,20)
+    ctx.fillText("B:"+angB,60,20)
+    ctx.fillText("C:"+angC,100,20)
 }
 
 function length(point1,point2){
@@ -55,12 +65,11 @@ function centroid(point1,point2,point3){
 }
 
 function scaleAndCentre(point1, point2, point3) {
-    const points = [point1,point2,point3]
     let leftmost;
     if (point1.x<= point2.x && point1.x <= point3.x){
-        leftmost = points[0]
+        leftmost = point1
     }else if (point2.x <= point1.x && point2.x <= point3.x){
-        leftmost = points[1]
+        leftmost = point2
     }else{
         leftmost = point3
     }
@@ -89,30 +98,51 @@ function scaleAndCentre(point1, point2, point3) {
         bottommost = point3
     }
     let midlr
-     
-    let midtb
-    
-    const height = points.bottommost.y - points.topmost.y
-    const width = points.rightmost.x - points.leftmost.x
-    const tall = height > width
-    const tbprop = (points.midtb.y - points.topmost.y)/height
-    const lrprop = (points.midlr.x - points.leftmost.x)/width
-    if (tall){
-        const scale = 250/height
-        points.topmost.y = 25
-        points.bottommost.y = 275
-        points.midtb.y = 25+(tbprop*250)
-        points.leftmost.x = 150-(width/2)
-        points.rightmost.x = 150 + (width/2)
-        points.midlr.x = points.rightmost.x + scale*lrprop
+    if ((leftmost == point1 && rightmost == point2) || (leftmost == point2 && rightmost == point1)){
+        midlr = point3
+    } else if ((leftmost == point1 && rightmost == point3) || (leftmost == point3 && rightmost == point1)){
+        midlr = point2
     } else {
-        const scale = 250/width
-        points.leftmost.x = 25
-        points.rightmost.x = 275
-        points.midlr.x = 25+(tbprop*250)
-        points.topmost.x = 150-(width/2)
-        points.bottommost.x = 150 + (width/2)
-        points.midtb.x = points.topmost.x + scale*tbprop
+        midlr = point1
+    }
+    let midtb
+    if ((topmost == point1 && bottommost == point2) || (topmost == point2 && bottommost == point1)){
+        midtb = point3
+    } else if ((topmost == point1 && bottommost == point3) || (topmost == point3 && bottommost == point1)){
+        midtb = point2
+    } else {
+        midtb = point1
+    }
+    console.log("Left: ("+leftmost.x+","+leftmost.y)
+    console.log("Right: ("+rightmost.x+","+rightmost.y) 
+    console.log("Top: ("+topmost.x+","+topmost.y) 
+    console.log("Bottom: ("+bottommost.x+","+bottommost.y) 
+    console.log("Mid lr: ("+midlr.x+","+midlr.y) 
+    console.log("Mid tb: ("+midtb.x+","+midtb.y) 
+    
+    const height = bottommost.y - topmost.y
+    const width = rightmost.x - leftmost.x
+    const prop = height/width
+    const tbprop = (midtb.y - topmost.y)/height
+    const lrprop = (midlr.x - leftmost.x)/width
+    if (prop > 1){
+        const newwid = 250/prop
+        console.log("newwid: "+newwid +"   lrprop: " + lrprop)
+        topmost.y = 25
+        bottommost.y = 275
+        midtb.y = 25+(tbprop*250)
+        leftmost.x = 150-(newwid/2)
+        rightmost.x = 150 + (newwid/2)
+        midlr.x = leftmost.x + newwid*lrprop
+    } else {
+        const newhei = 250*prop
+        console.log("newhei: "+newhei +"   tbprop: " + tbprop)
+        leftmost.x = 25
+        rightmost.x = 275
+        midlr.x = 25+(lrprop*250)
+        topmost.y = 150-(newhei/2)
+        bottommost.y = 150 + (newhei/2)
+        midtb.y = topmost.y + newhei * tbprop
     }
     
 }
